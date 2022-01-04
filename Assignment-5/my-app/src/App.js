e3dc98697a51e1010a199bc64224e8aaa7156885
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2022-01-04 19:32:52 +0530

    task_complete

--- Assignment-5/my-app/src/App.js
+++ Assignment-5/my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Landingpage from './LandingPage/landing_page';
+import Postview from './PostView/post_view';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="Wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landingpage />}/>
+          <Route path="/postViewPage" element={<Postview />}/>
+          
+        </Routes>
+
+      </BrowserRouter>
+
+      
     </div>
-  );
+
+);
 }
 
 export default App;
